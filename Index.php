526bd73bc7962684576e8557cc89996d73f7d176
oskarvinso <oxcarvincent@gmail.com>
2020-09-24 18:36:44 -0500

    optimizacion de visualizacion y colores, se incluyo el nombre de cada pkmon

--- Index.php
+++ Index.php
@@ -7,54 +7,49 @@
 <link rel="icon" type="image/png" href="CSS/images/polipkdexlogo.png">
 
 <?php
-$pokemon=1;
-$a=1;
+  $pokemon=1;
+  $a=1;
 
-function creartarjeta($pokemon) {
-  $api = curl_init("https://pokeapi.co/api/v2/pokemon/$pokemon");
-  curl_setopt($api, CURLOPT_SSL_VERIFYPEER, false);
-  curl_setopt($api, CURLOPT_RETURNTRANSFER, true);
-  $response = curl_exec($api);
-  curl_close($api);
+  function creartarjeta($pokemon) {
+    $api = curl_init("https://pokeapi.co/api/v2/pokemon/$pokemon");
+    curl_setopt($api, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($api, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($api);
+    curl_close($api);
 
-  $json = json_decode($response);
+    $json = json_decode($response);
 
-  echo '<h2>HABILIDADES</h2>';
-  foreach($json->abilities as $k => $v) {
-      echo $v->ability->name.'<br>';
+    echo '<h2>HABILIDADES</h2>';
+    foreach($json->abilities as $k => $v) {
+        echo $v->ability->name.'<br>';
+    }
+
+    echo '<h2>TIPO</h2>';
+    echo $json->types[0]->type->name;
+
+    echo '<h2>FOTOS</h2>';
+    echo '<img src="'.$json->sprites->back_default.'" width="200">';
+    echo '<img src="'.$json->sprites->front_default.'" width="200">';
+
   }
+  function creartarjetita($pokemon) {
+    $api = curl_init("https://pokeapi.co/api/v2/pokemon/$pokemon");
+    curl_setopt($api, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($api, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($api);
+    curl_close($api);
 
-  echo '<h2>TIPO</h2>';
-  echo $json->types[0]->type->name;
-
-  echo '<h2>FOTOS</h2>';
-  echo '<img src="'.$json->sprites->back_default.'" width="200">';
-  echo '<img src="'.$json->sprites->front_default.'" width="200">';
-
-}
-function creartarjetita($pokemon) {
-  $api = curl_init("https://pokeapi.co/api/v2/pokemon/$pokemon");
-  curl_setopt($api, CURLOPT_SSL_VERIFYPEER, false);
-  curl_setopt($api, CURLOPT_RETURNTRANSFER, true);
-  $response = curl_exec($api);
-  curl_close($api);
-
-  $json = json_decode($response);
-
-
-
-
-  echo '<button type="button" class="btn'.$pokemon.'">'.$json->name.'</button>';
-  echo '<style>.btn'.$pokemon.'{background:url('.$json->sprites->front_default.');background-position:center; background-repeat:no-repeat; padding:20px 15px;}</style>';
-  //echo '<span>'.$json->name.'</span>';
-}
+    $json = json_decode($response);
+    echo '<button type="button" class="btn"><img src="'.$json->sprites->front_default.'">'.$json->name.'</button>';
+  }
 ?>
+
 </head>
 <body>
 <div><img src="CSS/images/polipkdexlogo.png"></div>
 <div class="principalDiv">
   <?php
-  while ($a <= 10) {
+  while ($a <= 20) {
       creartarjetita($a);
       $a=$a+1;
   }
