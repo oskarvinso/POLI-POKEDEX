464728cc4a1203e331e79aa6089a5e0c561cb516
oskarvinso <oxcarvincent@gmail.com>
2020-09-24 17:32:35 -0500

    Update Index.php

--- Index.php
+++ Index.php
@@ -24,9 +24,16 @@ foreach($json->abilities as $k => $v) {
 echo '<h2>TIPO</h2>';
 echo $json->types[0]->type->name;
 
+<<<<<<< Updated upstream
 echo '<h2>FOTOS</h2>';
 echo '<img src="'.$json->sprites->back_default.'" width="200">';
 echo '<img src="'.$json->sprites->front_default.'" width="200">';
+=======
+  echo '<button type="button" class="btn'.$pokemon.'">'.$json->name.'</button>';
+  echo '<style>.btn'.$pokemon.'{background:url('.$json->sprites->front_default.');background-position:center; background-repeat:no-repeat; padding:20px 15px;}</style>';
+  //echo '<span>'.$json->name.'</span>';
+}
+>>>>>>> Stashed changes
 ?>
 
 </head>
